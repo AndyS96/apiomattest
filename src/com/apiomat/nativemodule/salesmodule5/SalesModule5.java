eaf0d151f1b4c2d0f3cedd21fb9921156d6febc2
AndyS96 <canexhd@gmail.com>
2018-11-07 11:21:10 +0100

    .

--- src/com/apiomat/nativemodule/salesmodule5/SalesModule5.java
+++ src/com/apiomat/nativemodule/salesmodule5/SalesModule5.java
@@ -35,19 +35,20 @@ package com.apiomat.nativemodule.salesmodule5;
  */
 @com.apiomat.nativemodule.Module( description="SalesModule5", 
          usedModules = {"Basics" }, securityPermissions = {} )
+@com.apiomat.nativemodule.NativeModuleConfig(
+		datatype = Type.LONG,
+		defaultValue = 100,
+		example = "100",
+		notifyAllNodes = true,
+		order = 1
+		)
 public class SalesModule5 implements com.apiomat.nativemodule.IModule
 {
     static com.apiomat.nativemodule.IApplicationConfigProxy APP_CONFIG_PROXY;
     static com.apiomat.nativemodule.IStaticMethods AOM;
     
     
-    @com.apiomat.nativemodule.NativeModuleConfig(
-    		datatype = Type.LONG,
-    		defaultValue = 100,
-    		example = "100",
-    		notifyAllNodes = true,
-    		order = 1
-    		)
+    
     // Sample for a module configuration
     //
     // @com.apiomat.nativemodule.NativeModuleConfig(
